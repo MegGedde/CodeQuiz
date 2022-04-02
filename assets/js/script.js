d37b3fcfddb8f215b1d8f792084bcd85824f114c
Megan Gedde <hoglu070@d.umn.edu>
2022-04-02 15:51:54 -0500

    added questions and additional sheets for highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@ var timerEl = document.querySelector("#timer");
 var questionTitle = document.querySelector("#question-title");
 var questionOptions = document.querySelector("#options");
 var feedbackAnswer = document.querySelector('#feedback');
+var submitBtn = document.querySelector('.submitbtn');
+var initial = document.querySelector('#initials');
 var setNumber = 0;
-var timeLeft = 60;
+var timeLeft = 6660;
 var timeInterval
 
 var myQuestions = [
@@ -21,6 +23,38 @@ var myQuestions = [
     { title: "Which tool can you use to ensure code quality?",
       choices: ["Angular", "jQuery", "RequireJS", "ESLint"],
       answer: "ESLint"
+    },
+    { 
+      title: "Javascript is an _____ language?",
+      choices: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+      answer: "Object-Oriented"
+    },
+    { title: "Which of the following methods is used to access HTML elements using Javascript?",
+      choices: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the above"],
+      answer: "Both A and B"
+    },
+    { 
+      title: "How can a datatype be declared to be a constant type?",
+      choices: ["const", "var", "let", "constant"],
+      answer: "const"
+    },
+    { title: "What keyword is used to check whether a given property is valid or not?",
+      choices: ["in", "is in", "exists", "lies"],
+      answer: "in"
+    },
+    { 
+      title: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+      choices: ["Boolean", "Undefined", "Object", "Integer"],
+      answer: "Object"
+    },
+    { title: "Which function is used to serialize an object into a JSON string in Javascript?",
+      choices: ["stringify()", "parse()", "convert()", "None of the above"],
+      answer: "stringify()"
+    },
+    { 
+      title: "Which of the following are closures in Javascript?",
+      choices: ["Variables", "Functions", "Objects", "All of the above"],
+      answer: "All of the above"
     }
   ];
 
@@ -103,12 +137,30 @@ var gameOver = function() {
     document.getElementById("questionTitles").style.display = "none";
     document.getElementById("scoring").style.display = "block";
     document.getElementById("finalScore").textContent = "Your score is " + timeLeft;
-     // save score 
-     //var finalScore = document.querySelector('#score')
 }      
 
-startBtn.addEventListener("click", startQuiz);
+var save = function () {
+  // save score
+   var initials = initial.value.trim();
+  // format new score object for current user
+  var highscores =
+  JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+  var newScore = {
+      score: timeLeft,
+      initials: initials
+  };
+   
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
+     // redirect to next page
+     window.location.href = "highScores.html";
+}
+
+startBtn.addEventListener("click", startQuiz);
+submitBtn.addEventListener("click", save);
 
 
 
