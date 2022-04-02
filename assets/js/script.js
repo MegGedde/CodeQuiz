258bd7c40c4c0c6bbea6e9021ee3b20c5ffe02a9
Megan Gedde <hoglu070@d.umn.edu>
2022-04-02 14:58:43 -0500

    created end screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,8 @@ var questionOptions = document.querySelector("#options");
 var feedbackAnswer = document.querySelector('#feedback');
 var setNumber = 0;
 var timeLeft = 60;
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
+var timeInterval
+
 var myQuestions = [
   {
     title: "Commonly Used data types DO NOT include:",
@@ -41,19 +33,19 @@ var startQuiz = function() {
     document.getElementById("questionTitles").style.display = "block";
       };
 
-// var lastQuestion = myQuestions.length -1;
 
 var getQuestion = function() {
+  //reset shown question and answers
     questionTitle.textContent = "";
     questionOptions.textContent = "";
-    // for (let i = 0; i < myQuestions.length; i++) {
+
       var question = document.createElement("h3");
       //get current question from array
       var currentQuestion = myQuestions[setNumber];
     //update title on site with current question
       question.textContent = currentQuestion.title;
       questionTitle.append(question);
-      // }
+      
       //loop over answer choices
     currentQuestion.choices.forEach(function(choice, i) {
       // create new button for each choice
@@ -92,9 +84,7 @@ var checkAnswer = function() {
 };
 
 var startTime = function() {
-        
-    
-        var timeInterval = setInterval(function () {
+        timeInterval = setInterval(function () {
           if (timeLeft > 0) {
             timerEl.textContent = 'Time: ' + timeLeft;
             timeLeft--;
@@ -107,48 +97,24 @@ var startTime = function() {
       }
 
 var gameOver = function() {
-        // edit title and message
-    // create write in box that saves to local storage
-    // save score in write in box?
-    //clearInterval(timeInterval);
-    // clear page contents
-    questionTitle.textContent = "Your score is" + timeLeft;
-    questionOptions.textContent = "";
-
-    // if (timeLeft > 0) {
-    //     something-.textContent = 'Your score is ' + timeLeft;
-    // } else {
-    //     something -.textContent = 'You do not have a score as you have lost the game.';
-    // }
+    //stop timer
+    clearInterval(timeInterval);
+    // clear page contents and add end page contents
+    document.getElementById("questionTitles").style.display = "none";
+    document.getElementById("scoring").style.display = "block";
+    document.getElementById("finalScore").textContent = "Your score is " + timeLeft;
+     // save score 
+     //var finalScore = document.querySelector('#score')
 }      
 
 startBtn.addEventListener("click", startQuiz);
-// on click, validate if it is correct, then go to next question
-//onclick.gotonextquestion event, re-render html to get question
-//have all answers as an array and the correct answer a common id?
-//event.target.value === questionarray.(if button text equals what we deem as the correct answer)
-// startBtn.addEventListener("click", function() {
-//create HTML elements to hold the data values
-// populateQuestion();
-// });
-
-// var populateQuestion = function() {
-//   questionTitle.textContent = "";
-//   for(let i = 0; i < myQuestions.length; i++) {
-//   var itemData = document.createElement("button");
-//   itemData.textContent = myQuestions[setNumber][i]
-//   //itemData.textContent = myQuestions[0][i]   ???
-//   questionTitle.append(itemData)
-//   //console.log("hi")
-// }
-// setNumber++;
-// };
 
 
-// questionTitle.addEventListener("click", function() {
-//   // delete current content
 
-//   //add new question
-//   populateQuestion();
-// });
+
+
+
+
+
+
 
